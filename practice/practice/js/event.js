78f259c31330558f11f2de6c4444e14f4268c12b
izumi0913-coder <yoshie050913@gmail.com>
2025-05-25 08:29:50 +0900

    5月25日練習分です

--- practice/practice/js/event.js
+++ practice/practice/js/event.js
@@ -1,3 +1,12 @@
+//ハンバーガーメニュー
+const ham = document.querySelector('#js-hamburger');
+const nav = document.querySelector('#js-nav');
+
+ham.addEventListener('click', () => {
+  ham.classList.toggle('active');
+  nav.classList.toggle('active');
+});
+
 // アコーディオンメニュー
 const qa = document.querySelectorAll(".js-ac"); 
 
